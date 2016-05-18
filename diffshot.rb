ae9a1aa444f4a05e33884ee2027c5f9c1d9f5d75
RobertAKARobin <robertgfthomas@gmail.com>
2016-05-18 12:07:22 -0400

    Shows all commits

--- diffshot.rb
+++ diffshot.rb
@@ -1,6 +1,20 @@
 require "mini_magick"
 require "yaml"
 
-c = YAML::load_file(File.join(__dir__, "config.yml"))
+c = YAML::load_file(File.join(__dir__, 'config.yml'))
 
-puts c.to_s
+def all_commits
+  output    = []
+  raw       = `git log --pretty=format:"%h%n%s"`.split("\n")
+  hash      = ""
+  raw.each_with_index do |line, linenum|
+    if linenum.even?
+      hash  = line
+    else
+      output.push({hash: hash, message: line})
+    end
+  end
+  return output
+end
+
+puts all_commits
